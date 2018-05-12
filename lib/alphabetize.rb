80d9b84772c0db8935516d7b0f86eb72efa7531c
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-12 20:28:49 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,6 @@
 def alphabetize(arr)
   # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+  arr.sort_by{|string| string.tr(esperanto_alphabet, ascii)}
 end
