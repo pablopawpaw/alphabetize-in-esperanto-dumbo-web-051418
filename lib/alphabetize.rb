fab2ddee9d9db774dbb5ded4174ddcf8641253e7
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-12 20:28:40 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,5 +3,4 @@ def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
   arr.sort_by{|string| string.tr(esperanto_alphabet, ascii)}
-
 end
